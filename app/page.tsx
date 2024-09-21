2856c9abba515b67783aa9d3c797b06d08e7cba1
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2024-09-21 06:03:51 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 "use client";
 import React from 'react';
-import NavBar from './_component/navbar.jsx';
+
 import HeroPage from './_component/hero.jsx';
 import TopRated from './_component/toprated.jsx';
 import HomeService from './_component/service.jsx';
 import Review from './_component/review.jsx';
 import OurTeam from './_component/ourTeam.jsx';
-import Footer from './_component/footer.jsx';
+
 import Image from "next/image";
 
 export default function Home() {
   
   return (
     <div className="">
-<NavBar />
+
 <HeroPage />
 <TopRated />
     <HomeService /> 
       <Review />  
       <OurTeam /> 
-      <Footer />   
+      
 
 
 
