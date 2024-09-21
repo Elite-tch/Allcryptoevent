f4cced552b1eab62cc740f0c05dc288d4b0b0dd5
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2024-09-21 17:17:11 +0100

    Update navbar.jsx

--- app/_component/navbar.jsx
+++ app/_component/navbar.jsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from 'next/image';
-
 import React, { useState } from 'react';
 import Link from 'next/link';
 
@@ -9,14 +8,17 @@ function NavBar() {
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
+  // Close menu when a link is clicked
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <div  
-    className=" w-full shadow-md bg-gray-900 text-white fixed top-0 left-0 z-50">
+      className="w-full shadow-md bg-gray-900 text-white fixed top-0 left-0 z-50">
       <nav className="flex items-center justify-between p-4">
         <div className="navbar-brand flex items-center">
           <Link href="/" className="flex items-center">
             <Image alt="logo" src="/logocc1.png" width={80} height={80} />
-            <h1 className="text-2xl text-white  font-bold">ACEHub</h1>
+            <h1 className="text-2xl text-white font-bold">ACEHub</h1>
           </Link>
         </div>
 
@@ -42,28 +44,29 @@ function NavBar() {
             </svg>
           </button>
         </div>
-  <ul
+
+        <ul
           className={`lg:flex lg:items-center lg:space-x-4 lg:space-y-0 space-y-4 lg:pt-0 pt-6 fixed inset-0 bg-gray-900 lg:bg-transparent lg:relative lg:flex-row lg:top-auto lg:left-auto top-16 left-0 w-full lg:w-auto transition-transform transform ${
-           isOpen ? 'translate-x-0' : '-translate-x-full'
+            isOpen ? 'translate-x-0' : '-translate-x-full'
           } z-40`} >
         
           <li className="px-4 py-2 lg:px-2">
-            <Link href="/event" className="hover:bg-gray-800 hover:p-2 hover:rounded transition text-white">
+            <Link href="/event" className="hover:bg-gray-800 hover:p-2 hover:rounded transition text-white" onClick={closeMenu}>
               Events
             </Link>
           </li>
           <li className="px-4 py-2 lg:px-2">
-            <Link href="/services" className="hover:bg-gray-800 hover:p-2 hover:rounded transition text-white">
+            <Link href="/services" className="hover:bg-gray-800 hover:p-2 hover:rounded transition text-white" onClick={closeMenu}>
               Services
             </Link>
           </li>
           <li className="px-4 py-2 lg:px-2">
-            <Link href="/about" className="hover:bg-gray-800 hover:p-2 hover:rounded transition text-white">
+            <Link href="/about" className="hover:bg-gray-800 hover:p-2 hover:rounded transition text-white" onClick={closeMenu}>
               About
             </Link>
           </li>
           <li className="px-4 py-2 lg:px-2">
-            <Link href="/blogs" className="hover:bg-gray-800 hover:p-2 hover:rounded transition text-white">
+            <Link href="/blogs" className="hover:bg-gray-800 hover:p-2 hover:rounded transition text-white" onClick={closeMenu}>
               Blogs
             </Link>
           </li>
@@ -71,6 +74,7 @@ function NavBar() {
             <Link
               href="/login"
               className="group relative inline-block text-sm font-medium text-white focus:outline-none active:text-indigo-500"
+              onClick={closeMenu}
             >
               <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-x-0 group-hover:translate-y-0 hover:border-indigo-500"></span>
               <span className="relative block border border-current bg-gray-800 px-3 py-2">Log In</span>
@@ -80,6 +84,7 @@ function NavBar() {
             <Link
               href="/signup"
               className="group relative inline-block text-sm font-medium text-white focus:outline-none active:text-indigo-500"
+              onClick={closeMenu}
             >
               <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-x-0 group-hover:translate-y-0 hover:border-indigo-500"></span>
               <span className="relative block border border-current bg-gray-800 px-3 py-2">Sign up</span>
@@ -92,4 +97,3 @@ function NavBar() {
 }
 
 export default NavBar;
-    
